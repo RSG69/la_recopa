1992d1cba8ed7111799352aaca60da5a0438795a
RSG69 <69rsg69@gmail.com>
2025-11-27 20:30:36 +0100

    Fix Reflex single port on Railway

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 apt-get update && apt-get install -y unzip curl
 export PORT=${PORT:-3000}
-reflex run --env prod --backend-host 0.0.0.0 --backend-port $PORT
+reflex run --env prod --backend-host 0.0.0.0 --single-port
+
 
 
 
