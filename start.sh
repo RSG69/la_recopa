94c1158f5701992ac464b00c6a3ff4e4b0353cba
RSG69 <69rsg69@gmail.com>
2025-11-27 20:19:40 +0100

    Disable events for production - revertirlo-

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update && apt-get install -y unzip curl
 export PORT=${PORT:-3000}
-reflex run --env prod --backend-host 0.0.0.0 --backend-port $PORT --disable-events
+reflex run --env prod --backend-host 0.0.0.0 --backend-port $PORT
 
 
 
