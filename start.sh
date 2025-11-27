9923e9071c32d262b134f9df70598fe3fb7652b4
RSG69 <69rsg69@gmail.com>
2025-11-27 20:12:31 +0100

    Disable events for production

--- start.sh
+++ start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-# Instalar dependencias necesarias
 apt-get update && apt-get install -y unzip curl
-
-# Railway asigna un puerto dinámico. Si no está, usamos 3000.
 export PORT=${PORT:-3000}
+reflex run --env prod --backend-host 0.0.0.0 --backend-port $PORT --disable-events
 
-# Iniciar Reflex usando el puerto asignado por Railway
-reflex run --env prod --backend-host 0.0.0.0 --backend-port $PORT
 
 
 
