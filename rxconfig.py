e1d9f1c0696708dd25cdc61b918485c44e55820c
RSG69 <69rsg69@gmail.com>
2025-11-27 20:48:00 +0100

    quitar marca de agua de reflex

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="la_recopa",
+    show_built_with_reflex=False
+    
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
