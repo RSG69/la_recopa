e4056e6db2d068d6a5ce085a020fd7ff6d433212
RSG69 <69rsg69@gmail.com>
2025-11-27 21:00:53 +0100

    revertir marca de agua de reflex

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="la_recopa",
-    show_built_with_reflex=False
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
