8386251cbb4cf92cd81f5954cec92d251938d980
RSG69 <69rsg69@gmail.com>
2025-11-27 19:28:05 +0100

    Fix Railway curl + unzip

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-apt-get update && apt-get install -y unzip
+
+# Actualizar paquetes e instalar dependencias necesarias de Reflex
+apt-get update && apt-get install -y unzip curl
+
+# Ejecutar Reflex en modo producción
 reflex run --env prod
+
