1543633b405a37084b37bc225046f28450c93fc9
RSG69 <69rsg69@gmail.com>
2025-11-27 19:59:56 +0100

    Fix backend-port for Railway

--- start.sh
+++ start.sh
@@ -6,8 +6,9 @@ apt-get update && apt-get install -y unzip curl
 # Railway asigna un puerto dinámico. Si no está, usamos 3000.
 export PORT=${PORT:-3000}
 
-# Iniciar Reflex usando un solo puerto (frontend + backend + websocket)
-reflex run --env prod --backend-host 0.0.0.0 --single-port $PORT
+# Iniciar Reflex usando el puerto asignado por Railway
+reflex run --env prod --backend-host 0.0.0.0 --backend-port $PORT
+
 
 
 
